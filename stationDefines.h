80f2e28762b42597993a97e974cf99ed8e67f027
Diego Silva <dsmelkor@gmail.com>
2019-05-17 16:23:42 -0300

    limpieza de codigo y cambio de nombre
    Al usar un sistema de GITHUB y el IDE de ARDUINO tuve que realizar cambios en el nombre del archivo y ademas limpiar el cod que descargamos

--- stationDefines.h
+++ stationDefines.h
@@ -1,13 +1,10 @@
 <<<<<<< HEAD
 
+
+
 /* Parametros de Control Automatico */
 #define DRY_SOIL      15
 #define WET_SOIL      66
-=======
-/* Automatic Control Parameters Definition */
-#define DRY_SOIL      20
-#define WET_SOIL      45
->>>>>>> e0f7f387a2edd7aa77bebf8762b23e9be140b082
 #define COLD_TEMP     12
 #define HOT_TEMP      26
 #define TIME_PUMP_ON  30
@@ -18,7 +15,6 @@
 uint32_t SLEEP_TIME = 140e6;  // Tiempo en modo deep-sleep en microsegundos
 
 /* TIMER */
-<<<<<<< HEAD
 #define READ_BUTTONS_TM   1L  // el termino L corresponde a Segundos
 #define READ_SOIL_TEMP_TM 2L  // 2L = 2 segundos 
 #define READ_SOIL_HUM_TM  10L
@@ -26,14 +22,7 @@ uint32_t SLEEP_TIME = 140e6;  // Tiempo en modo deep-sleep en microsegundos
 #define SEND_UP_DATA_TM   10L
 #define AUTO_CTRL_TM      60L     
 #define DEEP_SLEEP_TM     300L  
-=======
-#define READ_BUTTONS_TM   1L  // definitions in seconds
-#define READ_SOIL_TEMP_TM 20L
-#define READ_SOIL_HUM_TM  20L
-#define READ_AIR_DATA_TM  20L
-#define SEND_UP_DATA_TM   180L
-#define AUTO_CTRL_TM      180L     
->>>>>>> e0f7f387a2edd7aa77bebf8762b23e9be140b082
+
 
 /* OLED */
 boolean turnOffOLED = 1;
@@ -50,13 +39,9 @@ float airTemp = 0;
 #define soilMoisterVcc D8
 int soilMoister = 0;
 
-<<<<<<< HEAD
+
 /* DS18B20 Sensor de Temperatura */
 #define ONE_WIRE_BUS 14 // DS18B20 en NodeMCU pin D5 corresponde to GPIO 014 on Arduino
-=======
-/* DS18B20 Temperature Sensor */
-#define ONE_WIRE_BUS 14 // DS18B20 on NodeMCU pin D5 corresponds to GPIO 014 on Arduino
->>>>>>> e0f7f387a2edd7aa77bebf8762b23e9be140b082
 float soilTemp;
 
 /* Relays */
@@ -65,14 +50,8 @@ float soilTemp;
 boolean pumpStatus = 0;
 boolean lampStatus = 0;
 
-<<<<<<< HEAD
+
 /* Botones */
 #define PUMP_ON_BUTTON D9        //push-button BOMBA (Red)
 #define LAMP_ON_BUTTON D10       //push-button LAMPARA (Green)
 #define SENSORS_READ_BUTTON D4   //push-button SENSOR (White)
-=======
-/* Buttons */
-#define PUMP_ON_BUTTON D9        //push-button PUMP (Red)
-#define LAMP_ON_BUTTON D10       //push-button LAMP (Green)
-#define SENSORS_READ_BUTTON D4   //push-button SENSOR (yellow)
->>>>>>> e0f7f387a2edd7aa77bebf8762b23e9be140b082
